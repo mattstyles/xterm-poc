ac15792dbfc52b4a4a3bad2fef14a33554f49bee
Matt Styles <matt@personalurban.com>
2019-07-02 22:14:41 +0100

    update muck with font settings

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Terminal } from 'xterm'
 import * as fit from 'xterm/lib/addons/fit/fit'
 import chalk from 'chalk'
 import Stats from 'stats.js'
+import { random } from 'lodash/fp'
 
 import 'xterm/lib/xterm.css'
 // import 'xterm/lib/addons/fit/fit.css'
@@ -20,7 +21,10 @@ document.body.appendChild(stats.dom)
 Terminal.applyAddon(fit)
 
 const term = new Terminal({
-  experimentalCharAtlas: 'static'
+  rendererType: 'canvas',
+  experimentalCharAtlas: 'dynamic',
+  fontFamily: 'DejaVu Sans Mono',
+  fontSize: 18
 })
 term.open(el)
 window.term = term
@@ -40,6 +44,25 @@ term.focus()
 
 term.writeln('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 term.writeln('Next line')
+term.writeln('\nThe year was 1986.')
+term.writeln('He was a teenager like any other')
+term.writeln('Dreaming of his heroes and in love with a girl')
+term.writeln('But on a thunderous night along a ragged coast')
+term.writeln('A mysterious red car came to him')
+term.writeln('Its power lighting his eyes blood-red')
+term.writeln('In a flash all was lost in the hellfire of twisted metal')
+term.writeln('When our hero emerged from the burning wreckage')
+term.writeln('He and the car had become one, their souls spliced forever')
+term.writeln('Leaving him to wander the night alone\n')
+// term.writeln('Invisible to everyone but her')
+
+var ln = 'Invisible to everyone but her\r\n'.split('')
+const printLn = (term) => () => {
+  term.write(ln.shift())
+  setTimeout(printLn(term), 1000 / random(5, 60))
+}
+
+setTimeout(printLn(term), 500)
 
 term.prompt = () => {
   // term.write('\r\n$ ')
@@ -78,5 +101,5 @@ const writeScreen = (term) => () => {
   stats.end()
 }
 
-setTimeout(writeScreen(term), 1000)
+// setTimeout(writeScreen(term), 3000)
 // window.requestAnimationFrame(writeScreen(term))
